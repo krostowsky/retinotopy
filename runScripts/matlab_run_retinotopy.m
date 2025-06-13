c391100ea4d3f045421f4beb1e73de4c82a81f53
Kenneth Rostowsky <kennethrostowsky@gmail.com>
2025-06-13 15:29:18 -0500

    clear

--- runScripts/matlab_run_retinotopy.m
+++ runScripts/matlab_run_retinotopy.m
@@ -1,21 +1,44 @@
 clc; clear;
+addpath(genpath('/project/joelvoss/tmp-rostowsky/github/retinotopy'));
+
 subjectDir = '/project/joelvoss/tmp-rostowsky/hpcData';
 indir = '/project/joelvoss/data';
 outdir = '/project/joelvoss/tmp-rostowsky/hpcData';
 codedir = '/project/joelvoss/code';
 
 pathToImages = '/project/joelvoss/code/retinotopy/workspace_retinotopyCaltsmash.mat';
-subjects = struct2cell(dir(subjectDir));
+subjects = struct2cell(dir(indir));
 subjects = subjects(1,3:end);
 
-%% step 1
+skipSubjects = {'NU001', 'NU004', 'UC001', 'UC008'};
+
+%% if subjects are already processed or have pre-defined problems, skip
+for j = length(subjects):-1:1
+    if ~exist([indir '/' subjects{j} '/behav/retinotopy/']) || exist([outdir '/' subjects{j} '/tfAnalysis.mat']) ...
+            || ismember(subjects{j}, skipSubjects)
+        subjects(j) = [];
+    end
+end
+
+%% step 0
 for j = 1:length(subjects)
-    epochData(subjects{j}, indir,[outdir '/' subjects{j}], codedir, pathToImages);
+    if ~exist([outdir '/' subjects{j}])
+        mkdir([outdir '/' subjects{j}]);
+    end
+    if ~exist([outdir '/' subjects{j} '/logs'])
+        mkdir([outdir '/' subjects{j} '/logs']);
+    end
+end
+
+%% step 1
+% align data
+for j = 6:length(subjects)
+    epochData(subjects{j}, indir, [outdir '/' subjects{j}], codedir, pathToImages);
 end
 
 %%
 figdir = '/project/joelvoss/tmp-rostowsky/hpcDataFigs';
-dlDir = '/project/joelvoss/tmp-rostowsky/dlDir'; % this is directory that you can download onto your local machine to look at the images 
+dlDir = '/project/joelvoss/tmp-rostowsky/dlDir'; % this is directory that you can download onto your local machine to look at the images
 for j = 1:length(subjects)
     mkdir([dlDir '/' subjects{j} '/epoched/' ]);
     [resultLabel] = matchSubjectToHpcElectrodes(subjects{j});
